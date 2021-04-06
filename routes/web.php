935d262f7c566e64d67514888d11c607f4dc4a05
Juan Pablo <munozsjp@gmail.com>
2021-04-05 21:04:26 -0400

    Modelo/Controller/vista Items

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+// Items Routes
+Route::get('/items', 'ItemController@index')->name('items.index');
